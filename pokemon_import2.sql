3ddac94014feca30ae7304a5ccdc350a7706dc87
KatWei67 <129415169+KatWei67@users.noreply.github.com>
2023-04-19 11:25:33 -0700

    Update pokemon_import2.sql

--- pokemon_import2.sql
+++ pokemon_import2.sql
@@ -1,4 +1,5 @@
-CREATE TABLE pokemon(
+--TO achieve 2NF, create pokemon_2NF, classfication, type1, type2 tables 
+CREATE TABLE pokemon_2NF(
   against_bug REAL, against_dark REAL, against_dragon REAL, against_electric REAL,
   against_fairy REAL, against_fight REAL, against_fire REAL, against_flying REAL,
   against_ghost REAL, against_grass REAL, against_ground REAL, against_ice REAL,
@@ -9,39 +10,29 @@ CREATE TABLE pokemon(
   percentage_male REAL, pokedex_number INTEGER, sp_attack INTEGER,
   sp_defense INTEGER, speed INTEGER, weight_kg REAL,
   generation INTEGER, is_legendary INTEGER);
-
-CREATE TABLE ability(pokedex_number INTEGER, abilities TEXT);
-
+ 
 CREATE TABLE classfication(pokedex_number INTEGER, classfication TEXT);
 
 CREATE TABLE type1(pokedex_number INTEGER, type1 TEXT);
 
 CREATE TABLE type2(pokedex_number INTEGER, type2 TEXT);
 
-INSERT INTO pokemon(against_bug, against_dark, against_dragon, against_electric,
-  against_fairy, against_fight, against_fire, against_flying,
-  against_ghost, against_grass, against_ground, against_ice,
-  against_normal, against_poison, against_psychic, against_rock,
-  against_steel, against_water, attack, base_egg_steps,
-  base_happiness, base_total, capture_rate,
-  defense, experience_growth, height_m, hp, name,
-  percentage_male, pokedex_number, sp_attack,
-  sp_defense, speed, weight_kg,
-  generation, is_legendary) SELECT against_bug, against_dark, against_dragon, against_electric,
-  against_fairy, against_fight, against_fire, against_flying,
-  against_ghost, against_grass, against_ground, against_ice,
-  against_normal, against_poison, against_psychic, against_rock,
-  against_steel, against_water, attack, base_egg_steps,
-  base_happiness, base_total, capture_rate,
-  defense, experience_growth, height_m, hp, name,
-  percentage_male, pokedex_number, sp_attack,
-  sp_defense, speed, weight_kg,
-  generation, is_legendary FROM imported_pokemon_data;
+--INSERT data
+INSERT INTO pokemon_2NF SELECT against_bug, against_dark  , against_dragon  , against_electric  ,
+  against_fairy  , against_fight  , against_fire  , against_flying  ,
+  against_ghost  , against_grass  , against_ground  , against_ice  ,
+  against_normal  , against_poison  , against_psychic  , against_rock  ,
+  against_steel  , against_water  , attack  , base_egg_steps  ,
+  base_happiness  , base_total  , capture_rate  ,
+  defense  , experience_growth  , height_m  , hp  , name  ,
+  percentage_male  , pokedex_number  , sp_attack  ,
+  sp_defense  , speed  ,  weight_kg  ,
+  generation  , is_legendary   FROM pokemon
   
-INSERT INTO ability(pokedex_number, abilities) SELECT pokedex_number, abilities FROM imported_pokemon_data;
-
 INSERT INTO classfication(pokedex_number, classfication) SELECT pokedex_number, classfication FROM imported_pokemon_data;
 
 INSERT INTO type1(pokedex_number, type1) SELECT pokedex_number, type1 FROM imported_pokemon_data;
 
 INSERT INTO type2(pokedex_number, type2) SELECT pokedex_number, type1 FROM imported_pokemon_data;
+
+
