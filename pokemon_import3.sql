7e1a3a8c73ddf9d5e9bbe8d6bb10489e99f4add0
KatWei67 <129415169+KatWei67@users.noreply.github.com>
2023-04-25 00:04:29 -0700

    Update pokemon_import3.sql

--- pokemon_import3.sql
+++ pokemon_import3.sql
@@ -1,34 +1,18 @@
-CREATE TABLE pokemon_3NF( height_m REAL, hp INTEGER, name TEXT, percentage_male REAL, pokedex_number INTEGER,  weight_kg REAL, speed INTEGER, generation INTEGER, is_legendary INTEGER);
+
+--TO achieve 3NF, create pokemon_3nf, base, and attribute tables 
+CREATE TABLE pokemon_3nf(pokedex_number INTEGER, name TEXT, classification TEXT,  base_total INTEGER, capture_rate INTEGER, generation INTEGER, is_legendary INTEGER); 
+
+CREATE TABLE base(base_total INTEGER, base_egg_steps INTEGER, base_happiness INTEGER);
+
+CREATE TABLE attribute( classification TEXT, attack INTEGER,sp_attack INTEGER, defense INTEGER, sp_defense INTEGER, percentage_male REAL, speed INTEGER, weight_kg REAL,experience_growth INTEGER, height_m REAL, hp INTEGER);
+
+--insert data to pokeon_3nf table
+INSERT INTO pokemon_3nf SELECT pokedex_number, name, classfication, base_total, capture_rate, generation, is_legendary FROM imported_pokemon_data;
+
+--insert data to base table
+INSERT INTO base SELECT base_total, base_egg_steps, base_happiness FROM imported_pokemon_data;
+
+--insert data to attribute table
+INSERT INTO attribute SELECT classfication, attack, sp_attack, defense, sp_defense, percentage_male, speed, weight_kg, experience_growth, height_m, hp FROM imported_pokemon_data;
 
 
-
-CREATE TABLE against(pokedex_number INTEGER, against_bug REAL, against_dark REAL, against_dragon REAL, against_electric REAL, against_fairy REAL, against_fight REAL, against_fire REAL, against_flying REAL, against_ghost REAL, against_grass REAL, against_ground REAL, against_ice REAL, against_normal REAL, against_poison REAL, against_psychic REAL, against_rock REAL, against_steel REAL, against_water REAL);
-
-CREATE TABLE attack(pokedex_number INTEGER, attack INTEGER, sp_attack INTEGER);
-
-CREATE TABLE defense(pokedex_number INTEGER, defense INTEGER, sp_defense INTEGER);
-
-CREATE TABLE base(pokedex_number INTEGER, base_egg_steps INTEGER, base_happiness INTEGER, base_total INTEGER);
-  
-CREATE TABlE capture(pokedex_number INTEGER, capture_rate INTEGER, experience_growth INTEGER);
-
-
-INSERT INTO pokemon_3NF(height_m, hp, name, percentage_male, pokedex_number,  weight_kg, speed, generation, is_legendary) 
-SELECT height_m, hp, name, percentage_male, pokedex_number,  weight_kg, speed, generation, is_legendary FROM pokemon;
-
-
-INSERT INTO against(pokedex_number, against_bug, against_dark, against_dragon, against_electric, against_fairy, against_fight, against_fire, against_flying, against_ghost, against_grass, against_ground, against_ice, against_normal, against_poison, against_psychic, against_rock, against_steel, against_water) SELECT pokedex_number, against_bug, against_dark, against_dragon, against_electric, against_fairy, against_fight, against_fire, against_flying, against_ghost, against_grass, against_ground, against_ice, against_normal, against_poison, against_psychic, against_rock, against_steel, against_water FROM pokemon;
-
-INSERT INTO attack(pokedex_number, attack, sp_attack) SELECT pokedex_number, attack, sp_attack FROM pokemon;
-
-INSERT INTO defense(pokedex_number, defense, sp_defense) SELECT pokedex_number, defense, sp_defense FROM pokemon;
-
-INSERT INTO base(pokedex_number, base_egg_steps, base_happiness, base_total) SELECT pokedex_number, base_egg_steps, base_happiness, base_total FROM pokemon;
-
-INSERT INTO capture(pokedex_number, capture_rate, experience_growth) SELECT pokedex_number, capture_rate, experience_growth FROM pokemon;
-
-  
- 
- 
- 
- 
